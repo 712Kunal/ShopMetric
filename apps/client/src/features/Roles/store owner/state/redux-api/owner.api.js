ff9741e585c010ca9a1c30a226ca31b07df4ac3c
712Kunal <jadhavkun0@gmail.com>
2026-05-18 01:40:15 +0530

    added store owner api

---
+++ apps/client/src/features/Roles/store owner/state/redux-api/owner.api.js
@@ -0,0 +1,19 @@
+import { createApi } from '@reduxjs/toolkit/query/react';
+import { baseQueryWithReauth } from '@/shared/state/redux-api/base.api.js';
+
+export const ownerApi = createApi({
+  reducerPath: 'ownerApi',
+  baseQuery: baseQueryWithReauth,
+  tagTypes: ['Dashboard'],
+
+  endpoints: (builder) => ({
+    getDashboard: builder.query({
+      query: () => ({
+        url: 'storeOwner/getDashboard',
+        method: 'GET',
+      }),
+
+      providesTags: ['Dashboard'],
+    }),
+  }),
+});
